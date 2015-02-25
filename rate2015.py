018af4d2df3902597583142d8889fa579ed0b43d
medeotl <fasiellon@yahoo.it>
2015-02-25 20:13:04 +0100

    aggiunto rate2015.py

---
+++ rate2015.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# impostare delete-event di GtkWidget a onDeleteWindow in GLADE per la finestra
+from gi.repository import Gtk
+
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def calcolaRate(self, button):
+        print ("a Morris piace il pinnacchio")
+
+gui2betested = '/home/medeo/coding/abandoned/rate/rate2015/guimockup.ui'
+builder = Gtk.Builder()
+builder.add_from_file(gui2betested)
+builder.connect_signals(Handler())
+
+window = builder.get_object("window1")
+window.set_default_size(350,170)
+window.show_all()
+
+Gtk.main()
